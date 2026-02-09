bb6a95115dcc73c22afcfabddfd4d1df4c3c5ee0
Vedant Dayala <vedantdayala.me@gmail.com>
2026-02-10 00:09:04 +0530

    Configure environment variables

--- milkapp/settings.py
+++ milkapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%h#&w04fn^abyj23_bkp%m)b5(%i7)2$e#lwsc#sctokg48ozv'
+# SECRET_KEY = 'django-insecure-%h#&w04fn^abyj23_bkp%m)b5(%i7)2$e#lwsc#sctokg48ozv'
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-dev-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# DEBUG = False
+SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-dev-key')
 
 ALLOWED_HOSTS = ['*']
 
